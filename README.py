7a153260ab53d959c145ed3e599ad1d070c3ed28
theodoradiam100-rgb <theodoradiam100@gmail.com>
2026-02-07 18:13:36 +0200

    Initial commit: Guess the Number game

---
+++ README.py
@@ -0,0 +1,5 @@
+# Guess the Number Game
+
+# Simple Python game where 
+# you guess a number from 1 to 100 in 5 attempts.
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+number = random.randint(1,100)
+print("Guess the number")
+attemps = 0
+L=[]
+
+while attemps < 5:
+
+    a = int(input("Give a number :"))
+    if a in L:
+        print("You already tried this number!")
+        continue
+
+    L.append(a)
+    attemps+=1
+
+    if a == number:
+        print("Excellent! You found the number", number)
+        break
+
+    else:
+
+        if a<number:
+            print("The number is higher ")
+
+        elif a>number:
+            print("the number is lower")
+
+else:
+    print("Sorry, you didn't find the number. It was", number)
+
+print(L)
+
